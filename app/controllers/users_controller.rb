f0d0c13a896973824ca6c0936dc2397686911495
MakikoMinakawa <makimew@gmail.com>
2017-04-19 14:37:43 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show]
+  
   def show
-    @user = User,find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def new
